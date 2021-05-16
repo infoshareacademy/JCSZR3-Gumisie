8c76b93770e4e73ce53a8d65c5f4f3771dfda067
Jakub Tracz <jakub.tracz.1994@o2.pl>
2021-05-16 13:50:30 +0200

    Creates top level menu and log in functionality.

---
+++ Grafik/Login.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grafik_Console
+{
+    public class Login
+    {
+        private static string userMail;
+        private static string userPassword;
+        public static void AskForCredentials()
+        {
+            Console.WriteLine("Please provide your email:");
+            userMail = Console.ReadLine();
+            Console.WriteLine("Please provide you password:");
+            userPassword = Console.ReadLine();
+        }
+        public static bool CheckCredentials() => userMail == "123" && userPassword == "admin";
+    }
+}
