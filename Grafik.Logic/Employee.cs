b4c82cd003e89ff5527320afe6371a40d4573aa4
Przemek <przemyslaw.sawicki83@gmail.com>
2021-05-16 13:50:14 +0200

    Employee Class created

--- Grafik.Logic/Employee.cs
+++ Grafik.Logic/Employee.cs
@@ -2,7 +2,21 @@
 
 namespace Grafik_Logic
 {
-    public class Class1
+    public class Employee
     {
+        
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int PhoneNumber { get; set; }
+        public string EmailAddress { get; set; }
+
+        
+        public Employee(string firstName, string lastName, int phoneNumber, string emailAdress)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            PhoneNumber = phoneNumber;
+            EmailAddress = emailAdress;
+        }
     }
 }
