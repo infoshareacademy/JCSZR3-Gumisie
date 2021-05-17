d11825561628b329fd958f4bd106bedfbebcdfdb
artur.s.zolkiewski@gmail.com <Zhq@P$$7?hYeSpkJKnLD>
2021-05-17 22:34:16 +0200

    Modification of Timesheet class, adding fields and methods

--- Grafik.Logic/Timesheet.cs
+++ Grafik.Logic/Timesheet.cs
@@ -8,5 +8,34 @@ namespace Grafik_Logic
 {
     class Timesheet
     {
+        private DateTime _getCurrentDateTime = DateTime.Now;
+        
+        public int ID { get; set; }
+        public Employee Employee { get; set; }
+        public DateTime WorkingDate { get; set; } // working date
+        private DateTime Day { get; set; } // store days of the week in case working in weekdays
+        public int WorkingStartingHour { get; set; }
+        public int WorkingEndingHour { get; set; }
+        public DateTime Timestamp { get => _getCurrentDateTime; private set => _getCurrentDateTime = value; } // date of creation/modification of record
+
+        public string ShiftType { get; set; } //TO DO ENUM
+        private int DayWorkingHours { get; set; } // property to store amount hours Employee working in day
+
+        public Timesheet() {  }
+        public Timesheet(int id, Employee employee, DateTime workingDate, int startingHourShift, int endingHoursShift)
+        {
+            ID = id;
+            Employee = employee;
+            WorkingDate = workingDate;
+            WorkingStartingHour = startingHourShift;
+            WorkingEndingHour = endingHoursShift;
+        }
+        public void getDate() {
+            DateTime dateTime = DateTime.Now;
+        }
+        public void getDayOfTheWeek() { 
+        
+        }
+        public int calcaulteHoursForShift(int start, int end) => DayWorkingHours = end - start;
     }
 }
