df7b91c42e38fe022ce66f2c0d571549ab645afa
Jakub Tracz <jakub.tracz.1994@o2.pl>
2021-05-30 21:11:26 +0200

    Comment out log-in for debugging

--- Grafik/Program.cs
+++ Grafik/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Grafik_Console
 {
@@ -7,16 +8,16 @@ namespace Grafik_Console
         static void Main()
         {
             Banner.DrawTopBanner();
-            Login.AskForCredentials();
-            //log-in 
-            while (!Login.CheckCredentials())
-            {
-                Console.WriteLine("Wrong credentials. Please press any key and try again.");
-                Console.ReadLine();
-                Console.Clear();
-                Banner.DrawTopBanner();
-                Login.AskForCredentials();
-            }
+            //Login.AskForCredentials();
+            ////log-in 
+            //while (!Login.CheckCredentials())
+            //{
+            //    Console.WriteLine("Wrong credentials. Please press any key and try again.");
+            //    Console.ReadLine();
+            //    Console.Clear();
+            //    Banner.DrawTopBanner();
+            //    Login.AskForCredentials();
+            //}
             //application start point
             do
             {
@@ -24,7 +25,6 @@ namespace Grafik_Console
                 Banner.DrawTopBanner("Jakub"); //user name fetched from the database
                 MainMenu mainMenu = new();
                 mainMenu.ListMenu();
-
                 //check user's choice in the main menu and returns the sebmenu 
                 while (!Int32.TryParse(Console.ReadLine(), out int userChoice) || mainMenu.CheckMenuChoice(userChoice) == null)
                 {
@@ -33,9 +33,6 @@ namespace Grafik_Console
                     mainMenu.ListMenu();
                     Console.WriteLine("Provided value was invalid. Please choose one option from the menu.");
                 }
-
-
-
             } while (Console.ReadKey().Key != ConsoleKey.Escape);
         }
     }
