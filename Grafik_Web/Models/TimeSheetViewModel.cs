d1421478760245266e0d8cdc8278bc1a53ed4a40
DanielPrzewozny <daniel.przewozny@yahoo.com>
2021-08-10 21:55:48 +0200

    Bugfixes

---
+++ Grafik_Web/Models/TimeSheetViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grafik_Logic;
+
+namespace Grafik_Web.Models
+{
+    public class TimeSheetViewModel
+    {
+        public List<Timesheet> Timesheets;
+        public Timesheet Timesheet;
+
+        public List<Employee> Employees;
+        public Employee Employee;
+    }
+}
